faca99e73c53debfdf129910c3b84c927abf91d1
jungminkr9195 <130150639+jungminkr9195@users.noreply.github.com>
2024-11-26 13:53:29 +0900

    Update PSM.sh

--- scripts/PSM.sh
+++ scripts/PSM.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES='0' 
 
-python main.py --mode train  --anomaly_ratio 0.5  --temperature 1.0  --dataset PSM  --data_path dataset/PSM  --input_c 25  --output_c 25
-python main.py --mode udt  --anomaly_ratio 0.5  --temperature 1.0  --a 0.05  --dataset PSM  --data_path dataset/PSM  --input_c 25  --output_c 25  --pretrained_model 20
+python main.py  --mode train  --anomaly_ratio 0.5  --temperature 1.0  --dataset PSM  --data_path dataset/PSM  --input_c 25  --output_c 25
+python main.py  --mode udt  --anomaly_ratio 0.5  --temperature 1.0  --a 0.05  --dataset PSM  --data_path dataset/PSM  --input_c 25  --output_c 25  --pretrained_model 20
