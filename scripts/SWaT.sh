d9d910761c7dc5c8b2c5343b598ec2520368f673
jungminkr9195 <130150639+jungminkr9195@users.noreply.github.com>
2024-11-26 13:53:09 +0900

    Update SWaT.sh

--- scripts/SWaT.sh
+++ scripts/SWaT.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES='0'  
 
-python main.py --mode train --anomaly_ratio 5  --num_epochs 10  --dataset SWaT  --data_path dataset/SWaT  --input_c 51  --output_c 51
-python main.py --mode udt  --anomaly_ratio 5  --a 0.001  --dataset SWaT  --data_path dataset/SWaT  --input_c 51  --output_c 51  --pretrained_model 20
+python main.py  --mode train --anomaly_ratio 5  --num_epochs 10  --dataset SWaT  --data_path dataset/SWaT  --input_c 51  --output_c 51
+python main.py  --mode udt  --anomaly_ratio 5  --a 0.001  --dataset SWaT  --data_path dataset/SWaT  --input_c 51  --output_c 51  --pretrained_model 20
