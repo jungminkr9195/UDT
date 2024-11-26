7c944690040a78d78a08a031dd6146a2c3327dd4
jungminkr9195 <130150639+jungminkr9195@users.noreply.github.com>
2024-11-26 13:48:36 +0900

    Update SMD.sh

--- scripts/SMD.sh
+++ scripts/SMD.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES='0' 
 
-python main.py --mode train  --anormly_ratio 0.5  --temperature 1.5  --num_epochs 10  --dataset SMD  --data_path dataset/SMD  --input_c 38  --seed 0
-python main.py --mode udt  --anormly_ratio 0.5  --a 0.005  --temperature 1.5  --dataset SMD  --data_path dataset/SMD  --input_c 38  --output_c 38  --pretrained_model 20  --seed 0
+python main.py --mode train  --anormly_ratio 0.5  --temperature 1.5  --num_epochs 10  --dataset SMD  --data_path dataset/SMD  --input_c 38  --output_c 38
+python main.py --mode udt  --anormly_ratio 0.5  --a 0.005  --temperature 1.5  --dataset SMD  --data_path dataset/SMD  --input_c 38  --output_c 38  --pretrained_model 20
